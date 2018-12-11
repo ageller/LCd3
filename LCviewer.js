2b1f0d4d6ebd818e9023765df9d48ddc2507f858
Aaron Geller <ageller@dhcp-10-105-44-187.wireless.northwestern.private>
2018-12-11 13:23:08 -0600

    added error bars

--- LCviewer.js
+++ LCviewer.js
@@ -12,23 +12,24 @@ d3.csv("data/27882110006813.csv")
 //////////////
 // RAW DATA
 //////////////
-function createPlot(data, width, height, margin, xTitle, yTitle, left=0, top=0, fillColor="#DC143C"){
+function createPlot(data, width, height, margin, xTitle, yTitle, left=0, top=0, circleColor="#DC143C", errColor="#DC143C"){
 
+	var errLen = 4;
 
 	var x0 = [margin.left, width + margin.left ],
 		y0 = [height + margin.top, margin.top];
-	var x = d3.scaleLinear().range(x0),
-		y = d3.scaleLinear().range(y0);
+	var xScale = d3.scaleLinear().range(x0),
+		yScale = d3.scaleLinear().range(y0);
 
 
-	var xAxis = d3.axisBottom(x),
-		yAxis = d3.axisLeft(y);
+	var xAxis = d3.axisBottom(xScale),
+		yAxis = d3.axisLeft(yScale);
 
 	var xExtent = d3.extent(data, function(d) { return +d.x; }),
-		yExtent = d3.extent(data, function(d) { return +d.y; });
+		yExtent = d3.extent(data, function(d) { return (+d.y + d.ye); });
 
-	x.domain(xExtent).nice();
-	y.domain(yExtent).nice();
+	xScale.domain(xExtent).nice();
+	yScale.domain(yExtent).nice();
 
 	var plot = d3.select("body").append("svg")
 		.attr("width", (width + margin.left + margin.right))
@@ -60,15 +61,47 @@ function createPlot(data, width, height, margin, xTitle, yTitle, left=0, top=0, 
 		.style("text-anchor", "middle")
 		.text(yTitle)
 
+	// Add Error Line
+	plot.append("g").selectAll("line")
+		.data(data).enter()
+		.append("line")
+		.style("stroke", errColor)
+		.attr("class", "error-line")
+		.attr("x1", function(d) {return xScale(+d.x);})
+		.attr("y1", function(d) {return yScale(+d.y + d.ye);})
+		.attr("x2", function(d) {return xScale(+d.x);})
+		.attr("y2", function(d) {return yScale(+d.y - d.ye);});
 
-	var view = plot.selectAll(".dot")
+	// Add Error Top Cap
+	plot.append("g").selectAll("line")
+		.data(data).enter()
+		.append("line")
+		.style("stroke", errColor)
+		.attr("class", "error-cap error-cap-top")
+		.attr("x1", function(d) {return xScale(+d.x) - errLen;})
+		.attr("y1", function(d) {return yScale(+d.y + d.ye);})
+		.attr("x2", function(d) {return xScale(+d.x) + errLen;})
+		.attr("y2", function(d) {return yScale(+d.y + d.ye);});
+		
+	 // Add Error Bottom Cap
+	plot.append("g").selectAll("line")
+		.data(data).enter()
+		.append("line")
+		.style("stroke", errColor)
+		.attr("class", "error-cap error-cap-bottom")
+		.attr("x1", function(d) {return xScale(+d.x) - errLen;})
+		.attr("y1", function(d) {return yScale(+d.y - d.ye);})
+		.attr("x2", function(d) {return xScale(+d.x) + errLen;})
+		.attr("y2", function(d) {return yScale(+d.y - d.ye);});
+	
+	plot.selectAll(".dot")
 		.data(data)
 		.enter().append("circle")
 			.attr("class", "dot circle")
 			.attr("r", 3.5)
-			.attr("cx", function(d) { return x(+d.x); })
-			.attr("cy", function(d) { return y(+d.y); })
-			.style("fill", fillColor)
+			.attr("cx", function(d) { return xScale(+d.x); })
+			.attr("cy", function(d) { return yScale(+d.y); })
+			.style("fill", circleColor)
 			.style("opacity",0.7);
 
 
@@ -85,11 +118,11 @@ function createPlot(data, width, height, margin, xTitle, yTitle, left=0, top=0, 
 		var s = d3.event.selection;
 		if (!s) {
 			if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
-			x.domain(xExtent).nice();
-			y.domain(yExtent).nice();
+			xScale.domain(xExtent).nice();
+			yScale.domain(yExtent).nice();
 		} else {
-			x.domain([x.invert(s[0][0]), x.invert(s[1][0])]).nice();
-			y.domain([y.invert(s[1][1]), y.invert(s[0][1])]).nice();
+			xScale.domain([xScale.invert(s[0][0]), xScale.invert(s[1][0])]).nice();
+			yScale.domain([yScale.invert(s[1][1]), yScale.invert(s[0][1])]).nice();
 			plot.select(".brush").call(brush.move, null);
 		}
 		zoom();
@@ -104,8 +137,23 @@ function createPlot(data, width, height, margin, xTitle, yTitle, left=0, top=0, 
 		plot.select(".axis--x").transition(t).call(xAxis);
 		plot.select(".axis--y").transition(t).call(yAxis);
 		plot.selectAll("circle").transition(t)
-			.attr("cx", function(d) { return x(+d.x); })
-			.attr("cy", function(d) { return y(+d.y); })
+			.attr("cx", function(d) { return xScale(+d.x); })
+			.attr("cy", function(d) { return yScale(+d.y); });
+		plot.selectAll(".error-line").transition(t)
+			.attr("x1", function(d) {return xScale(+d.x);})
+			.attr("y1", function(d) {return yScale(+d.y + d.ye);})
+			.attr("x2", function(d) {return xScale(+d.x);})
+			.attr("y2", function(d) {return yScale(+d.y - d.ye);});
+		plot.selectAll(".error-cap-top").transition(t)
+			.attr("x1", function(d) {return xScale(+d.x) - errLen;})
+			.attr("y1", function(d) {return yScale(+d.y + d.ye);})
+			.attr("x2", function(d) {return xScale(+d.x) + errLen;})
+			.attr("y2", function(d) {return yScale(+d.y + d.ye);});
+		plot.selectAll(".error-cap-bottom").transition(t)
+			.attr("x1", function(d) {return xScale(+d.x) - errLen;})
+			.attr("y1", function(d) {return yScale(+d.y - d.ye);})
+			.attr("x2", function(d) {return xScale(+d.x) + errLen;})
+			.attr("y2", function(d) {return yScale(+d.y - d.ye);});
 	}
 
 	return plot;
@@ -121,16 +169,16 @@ function startPlotting(data){
 		width = 500,
 		height = 300;
 	data.forEach(function(d){
-		rawData.push({"x":d.hjd, "y":d.mag})
+		rawData.push({"x":parseFloat(d.hjd), "y":parseFloat(d.mag), "ye":parseFloat(d.emag)})
 	})
 	rawPlot = createPlot(rawData, width, height, margin, "Days", "Brightness");
 
 	var period = 0.46614771; //will be substituted with real data later
 	var phaseData = [];
 	data.forEach(function(d){
-		phaseData.push({"x": (d.hjd % period), "y":d.mag})
+		phaseData.push({"x":parseFloat(d.hjd) % period, "y":parseFloat(d.mag), "ye":parseFloat(d.emag)})
 	})
-	phasePlot = createPlot(phaseData, width, height, margin, "Phase", "Brightness", left=-(width + margin.left + margin.right), top=(height + margin.bottom + margin.top), fillColor="00ff00");
+	phasePlot = createPlot(phaseData, width, height, margin, "Phase", "Brightness", left=-(width + margin.left + margin.right), top=(height + margin.bottom + margin.top));
 
 
 	// console.log(phasePlot)
