affe8a8cdc62af71ddd3ff6a37514654e694d8c1
Aaron Geller <ageller@dhcp-10-105-44-187.wireless.northwestern.private>
2018-12-11 12:49:59 -0600

    working toward phase plot

--- LCviewer.js
+++ LCviewer.js
@@ -1,113 +1,133 @@
-var margin = {top: 0, right: 0, bottom: 60, left: 60},
-	width = 500,
-	height = 300,
-	allWidth = 500 + margin.left + margin.right,
-	allHeight = 300 + margin.top + margin.bottom,
-	idleTimeout,
+
+//some initial definitions
+var	idleTimeout,
 	idleDelay = 350;
 
-var x0 = [margin.left, allWidth - margin.right ],
-    y0 = [allHeight - margin.bottom, margin.top],
-    xExtent, yExtent;
-
-var x = d3.scaleLinear().range(x0);
-
-var y = d3.scaleLinear().range(y0);
-
-var xAxis = d3.axisBottom(x);
-
-var yAxis = d3.axisLeft(y);
-
-
-
-
-var rawLC = d3.select("body").append("svg")
-		.attr("width", allWidth)
-		.attr("height", allHeight)
 
 d3.csv("data/27882110006813.csv")
 	.then(function(data) {
+		startPlotting(data);
+	});
 
 //////////////
 // RAW DATA
 //////////////
-		xExtent = d3.extent(data, function(d) { return +d.hjd; });
-		yExtent = d3.extent(data, function(d) { return +d.mag; });
-
-		x.domain(xExtent).nice();
-		y.domain(yExtent).nice();
-
-		//axes
-		var gX = rawLC.append("g")
-			.attr("class", "axis axis--x")
-			.attr("transform", "translate(0," + (height + margin.top) + ")")
-			.call(xAxis)
-		var gY = rawLC.append("g")
-			.attr("transform", "translate(" + margin.left + ",0)")
-			.attr("class", "axis axis--y")
-			.call(yAxis)
-
-		//axes labels
-		rawLC.append("text")
-			.attr("class", "label")
-			.attr("x", width/2. + margin.left)
-			.attr("y", height + margin.bottom-20)
-			.style("text-anchor", "middle")
-			.text("Days");
-		rawLC.append("text")
-			.attr("class", "label")
-			.attr("transform", "rotate(-90)")
-			.attr("x", -height/2)
-			.attr("y", 20)
-			.style("text-anchor", "middle")
-			.text("Brightness")
+function createPlot(data, width, height, margin, xTitle, yTitle, left=0, top=0, fillColor="#DC143C"){
 
 
-		var rawLCview = rawLC.selectAll(".dot")
-			.data(data)
-			.enter().append("circle")
-				.attr("class", "dot circle")
-				.attr("r", 3.5)
-				.attr("cx", function(d) { return x(+d.hjd); })
-				.attr("cy", function(d) { return y(+d.mag); })
-				.style("opacity",0.7);
+	var x0 = [margin.left, width + margin.left ],
+		y0 = [height + margin.top, margin.top];
+	var x = d3.scaleLinear().range(x0),
+		y = d3.scaleLinear().range(y0);
+
+
+	var xAxis = d3.axisBottom(x),
+		yAxis = d3.axisLeft(y);
+
+	var xExtent = d3.extent(data, function(d) { return +d.x; }),
+		yExtent = d3.extent(data, function(d) { return +d.y; });
+
+	x.domain(xExtent).nice();
+	y.domain(yExtent).nice();
+
+	var plot = d3.select("body").append("svg")
+		.attr("width", (width + margin.left + margin.right))
+		.attr("height", (height + margin.top + margin.bottom))
+		.attr("transform", "translate(" + left + "," + top + ")")
+
+	//axes
+	var gX = plot.append("g")
+		.attr("class", "axis axis--x")
+		.attr("transform", "translate(0," + (height + margin.top) + ")")
+		.call(xAxis)
+	var gY = plot.append("g")
+		.attr("transform", "translate(" + margin.left + ",0)")
+		.attr("class", "axis axis--y")
+		.call(yAxis)
+
+	//axes labels
+	plot.append("text")
+		.attr("class", "label")
+		.attr("x", width/2. + margin.left)
+		.attr("y", height + margin.bottom-20)
+		.style("text-anchor", "middle")
+		.text(xTitle);
+	plot.append("text")
+		.attr("class", "label")
+		.attr("transform", "rotate(-90)")
+		.attr("x", -height/2)
+		.attr("y", 20)
+		.style("text-anchor", "middle")
+		.text(yTitle)
+
+
+	var view = plot.selectAll(".dot")
+		.data(data)
+		.enter().append("circle")
+			.attr("class", "dot circle")
+			.attr("r", 3.5)
+			.attr("cx", function(d) { return x(+d.x); })
+			.attr("cy", function(d) { return y(+d.y); })
+			.style("fill", fillColor)
+			.style("opacity",0.7);
 
 
 
-		//brush + zoom from here : https://bl.ocks.org/mbostock/f48fcdb929a620ed97877e4678ab15e6
-		var brush = d3.brush().on("end", brushended);
-		rawLC.append("g")
-			.attr("class", "brush")
-			.call(brush);
+	//brush + zoom from here : https://bl.ocks.org/mbostock/f48fcdb929a620ed97877e4678ab15e6
+	var brush = d3.brush().on("end", brushended);
+	plot.append("g")
+		.attr("class", "brush")
+		.call(brush);
 
-		function brushended() {
-			var s = d3.event.selection;
-			if (!s) {
-				if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
-				x.domain(xExtent).nice();
-				y.domain(yExtent).nice();
-			} else {
-				x.domain([x.invert(s[0][0]), x.invert(s[1][0])]).nice();
-				y.domain([y.invert(s[1][1]), y.invert(s[0][1])]).nice();
-				rawLC.select(".brush").call(brush.move, null);
-			}
-			zoom();
+
+	// helper functions for brushing and zooming
+	function brushended() {
+		var s = d3.event.selection;
+		if (!s) {
+			if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
+			x.domain(xExtent).nice();
+			y.domain(yExtent).nice();
+		} else {
+			x.domain([x.invert(s[0][0]), x.invert(s[1][0])]).nice();
+			y.domain([y.invert(s[1][1]), y.invert(s[0][1])]).nice();
+			plot.select(".brush").call(brush.move, null);
 		}
+		zoom();
+	}
 
-		function idled() {
-			idleTimeout = null;
-		}
+	function idled() {
+		idleTimeout = null;
+	}
 
-		function zoom() {
-			var t = rawLC.transition().duration(750);
-			rawLC.select(".axis--x").transition(t).call(xAxis);
-			rawLC.select(".axis--y").transition(t).call(yAxis);
-			rawLC.selectAll("circle").transition(t)
-				.attr("cx", function(d) { return x(+d.hjd); })
-				.attr("cy", function(d) { return y(+d.mag); })
-		}
+	function zoom() {
+		var t = plot.transition().duration(750);
+		plot.select(".axis--x").transition(t).call(xAxis);
+		plot.select(".axis--y").transition(t).call(yAxis);
+		plot.selectAll("circle").transition(t)
+			.attr("cx", function(d) { return x(+d.x); })
+			.attr("cy", function(d) { return y(+d.y); })
+	}
 
-	});
+	return plot;
+}
 
 
 
+
+//create the plots
+function startPlotting(data){
+	var rawData = [],
+		margin = {top: 0, right: 0, bottom: 60, left: 60},
+		width = 500,
+		height = 300;
+	data.forEach(function(d){
+		rawData.push({"x":d.hjd, "y":d.mag})
+	})
+	rawPlot = createPlot(rawData, width, height, margin, "Days", "Brightness");
+
+	var phaseData = rawData;
+	phasePlot = createPlot(phaseData, width, height, margin, "Phase", "Brightness", left=-(width + margin.left + margin.right), top=(height + margin.bottom + margin.top), fillColor="00ff00");
+
+
+	// console.log(phasePlot)
+}
